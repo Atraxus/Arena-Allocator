31eb631ceb154e7d403091de432e430a517757ef
Jannis Maier <jannismaier97@gmail.com>
2023-04-04 12:55:14 +0200

    first arena ideas

--- arena_allocator/src/arena.rs
+++ arena_allocator/src/arena.rs
@@ -2,7 +2,7 @@ use std::marker::PhantomData;
 
 struct InvariantLifetime<'id>(PhantomData<*mut &'id ()>);
 
-struct ArenaIndex<'id> { 
+pub struct ArenaIndex<'id> { 
     idx: usize,
     _marker: InvariantLifetime<'id> 
 }
@@ -12,6 +12,37 @@ pub struct Arena<'id, T> {
     _marker: InvariantLifetime<'id>,
 }
 
-// impl Arena<'id, T> {
-//     pub fn new<R>(inner:)
-// }
+impl<'id, T>  Arena<'id, T> {
+    pub fn new<R>(f: impl for<'a> FnOnce(Arena<'a, T>) -> R) -> R {
+        let inner: Vec<T> = Vec::new();
+        let arena = Arena { data: inner, _marker: InvariantLifetime(PhantomData) };
+        f(arena)
+    }
+
+    pub fn alloc(&mut self, val: T) -> usize {
+        let idx = self.data.len();
+        self.data.push(val);
+        idx
+    }
+
+    pub fn get_index(&mut self, idx: usize) -> Option<ArenaIndex<'id>> {
+        if idx < self.data.len() {
+            Some(ArenaIndex { idx, _marker: InvariantLifetime(PhantomData) })
+        } 
+        else {
+            None
+        }
+    }
+
+    pub fn get(&self, val: usize) -> &T {
+        &self.data[val]
+    }
+
+    pub fn get_mut(&mut self, val: usize) -> &mut T {
+        &mut self.data[val]
+    }
+
+    // pub fn free(&mut self, &T) {
+    //     &mut self.data[]
+    // }
+}
--- arena_allocator/src/main.rs
+++ arena_allocator/src/main.rs
@@ -4,7 +4,7 @@
 mod arena;
 mod branded_vec;
 
-// use arena::Arena;
+use arena::Arena;
 // use branded_vec::BrandedVec;
 
 struct Monster {
@@ -12,5 +12,13 @@ struct Monster {
 }
 
 fn main() {
-    println!("Hello, world!");
+    Arena::new(|mut db_monsters| Arena::new(|mut dc_monsters| {
+        let vegeta = db_monsters.alloc(Monster { level: 9001 });
+        assert!(db_monsters.get(vegeta).level > 9000);
+        // db_monsters.free(vegeta);
+        
+        let joker = dc_monsters.alloc(Monster { level: 100 });
+        dc_monsters.get_mut(joker).level = 101;
+        // db_monsters.get(&joker).level = 102; // doesn't compile!
+    }));
 }
