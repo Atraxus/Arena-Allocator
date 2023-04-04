5d5d29d686405e3d074a50f8cbf7d236bedf91fc
Jannis Maier <jannismaier97@gmail.com>
2023-04-04 15:02:40 +0200

    first working implementation w/o free

--- arena_allocator/src/arena.rs
+++ arena_allocator/src/arena.rs
@@ -2,47 +2,71 @@ use std::marker::PhantomData;
 
 struct InvariantLifetime<'id>(PhantomData<*mut &'id ()>);
 
-pub struct ArenaIndex<'id> { 
+pub struct ArenaToken<'id> { 
     idx: usize,
     _marker: InvariantLifetime<'id> 
 }
 
-pub struct Arena<'id, T> {
-    data: Vec<T>,
+pub struct ArenaCell<'id, T> {
+    value: T,
     _marker: InvariantLifetime<'id>,
 }
 
-impl<'id, T>  Arena<'id, T> {
-    pub fn new<R>(f: impl for<'a> FnOnce(Arena<'a, T>) -> R) -> R {
-        let inner: Vec<T> = Vec::new();
-        let arena = Arena { data: inner, _marker: InvariantLifetime(PhantomData) };
-        f(arena)
+impl<'id, T> ArenaCell<'id, T> {
+    /// Wraps some data T into a ArenaCell with brand `'id`.
+    fn new(value: T) -> Self { 
+        ArenaCell{value, _marker: InvariantLifetime(PhantomData)}
+    }
+    /// Turns an owned ArenaCell back into owned data.
+    fn into_inner(self) -> T { 
+        self.value
+    }
+    /// Turns a mutably borrowed ArenaCell to and from mutably borrowed data.
+    fn get_mut(&mut self) -> &mut T { 
+        &mut self.value
+    }
+    fn from_mut(t: &mut T) -> &mut Self { 
+        unsafe { &mut *(t as *mut T as *mut Self) } // transmute since we know the layout
     }
 
-    pub fn alloc(&mut self, val: T) -> usize {
-        let idx = self.data.len();
-        self.data.push(val);
-        idx
+    // Immutably borrows the ArenaCell with the same-branded token.
+    #[allow(unused_variables)]
+    fn borrow<'a>(&'a self, token: &'a ArenaToken<'id>) -> &'a T {
+        &self.value
+    }
+    /// Mutably borrows the ArenaCell with the same-branded token.
+    #[allow(unused_variables)]
+    fn borrow_mut<'a>(&'a self, token: &'a mut ArenaToken<'id>) -> &'a mut T {
+        unsafe { &mut *(self as *const Self as *mut Self) }.get_mut()
+    }
+}
+
+pub struct Arena<'arena, T> {
+    cells: Vec<ArenaCell<'arena, T>>,
+}
+
+impl<'arena, T> Arena<'arena, T> {
+    pub fn new<F, R>(f: F) -> R
+    where
+        F: FnOnce(&mut Arena<'arena, T>) -> R,
+    {
+        let mut arena = Arena { cells: Vec::new() };
+        let result = f(&mut arena);
+        arena.cells.clear();
+        result
+    }
+    
+    pub fn alloc(&mut self, value: T) -> ArenaToken<'arena> {
+        let idx = self.cells.len();
+        self.cells.push(ArenaCell::new(value));
+        ArenaToken { idx: idx, _marker: InvariantLifetime(PhantomData) }
     }
 
-    pub fn get_index(&mut self, idx: usize) -> Option<ArenaIndex<'id>> {
-        if idx < self.data.len() {
-            Some(ArenaIndex { idx, _marker: InvariantLifetime(PhantomData) })
-        } 
-        else {
-            None
-        }
+    pub fn get<'a>(&'a self, token: &'a ArenaToken<'arena>) -> &'a T {
+        self.cells[token.idx].borrow(token)
     }
 
-    pub fn get(&self, val: usize) -> &T {
-        &self.data[val]
+    pub fn get_mut<'a>(&'a self, token: &'a mut ArenaToken<'arena>) -> &'a mut T {
+        self.cells[token.idx].borrow_mut(token)
     }
-
-    pub fn get_mut(&mut self, val: usize) -> &mut T {
-        &mut self.data[val]
-    }
-
-    // pub fn free(&mut self, &T) {
-    //     &mut self.data[]
-    // }
 }
--- arena_allocator/src/main.rs
+++ arena_allocator/src/main.rs
@@ -1,6 +1,5 @@
 // Hide unused
 #![allow(dead_code)]
-
 mod arena;
 mod branded_vec;
 
@@ -12,13 +11,26 @@ struct Monster {
 }
 
 fn main() {
-    Arena::new(|mut db_monsters| Arena::new(|mut dc_monsters| {
+    Arena::new(|db_monsters| Arena::new(|dc_monsters| {
         let vegeta = db_monsters.alloc(Monster { level: 9001 });
-        assert!(db_monsters.get(vegeta).level > 9000);
+        assert!(db_monsters.get(&vegeta).level > 9000);
         // db_monsters.free(vegeta);
         
-        let joker = dc_monsters.alloc(Monster { level: 100 });
-        dc_monsters.get_mut(joker).level = 101;
-        // db_monsters.get(&joker).level = 102; // doesn't compile!
+        // In the following we want to test wheter we can get 
+        // mutable references to multiple monsters in the same arena.
+        let mut joker = dc_monsters.alloc(Monster { level: 100 });
+        let mut riddler = dc_monsters.alloc(Monster { level: 101 });
+        let mut penguin = dc_monsters.alloc(Monster { level: 102 });
+        let joker_inst = dc_monsters.get_mut(&mut joker);
+        let riddler_inst = dc_monsters.get_mut(&mut riddler);
+        let penguin_inst = dc_monsters.get_mut(&mut penguin);
+
+        joker_inst.level = 1000;
+        riddler_inst.level = 1001;
+        penguin_inst.level = 1002;
+
+        println!("Joker: {}", joker_inst.level);
+        println!("Riddler: {}", riddler_inst.level);
+        println!("Penguin: {}", penguin_inst.level);
     }));
 }
